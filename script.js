ed94bfaa38ad69e9e49c022b28f57d81610d6a67
THANTOEAUNG <kyawshein034@gmail.com>
2025-02-24 03:01:44 +0900

    responsive change

--- script.js
+++ script.js
@@ -108,3 +108,17 @@ contact.addEventListener('click', () => {
   skillContent.style.display = 'none'
   projectContent.style.display = 'none'
 })
+let lastScrollTop = 0;
+const header = document.querySelector(".header");
+
+window.addEventListener("scroll", function () {
+  let scrollTop = window.scrollY || document.documentElement.scrollTop;
+  
+  if (scrollTop > lastScrollTop) {
+    header.style.transform = "translateY(-100%)";
+  } else {
+    header.style.transform = "translateY(0)";
+  }
+
+  lastScrollTop = scrollTop;
+});
